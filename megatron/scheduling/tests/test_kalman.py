a3c3a12798d87739d0ed4ec3bd33955d619f0ef0
Matthew Rocklin <mrocklin@gmail.com>
2013-06-11 09:46:54 -0500

    test_kalman supports numeric dimensions

--- megatron/scheduling/tests/test_kalman.py
+++ megatron/scheduling/tests/test_kalman.py
@@ -1,10 +1,19 @@
 from sympy import Q, assuming
 from computations.matrices.examples.kalman import (inputs, outputs, n, k,
         assumptions)
-from computations.matrices.examples.kalman_comp import c
+from computations.matrices.examples.kalman_comp import make_kalman_comp
 from megatron.scheduling.times import make_compcost
 
 import numpy as np
+
+nn, nk = 1000, 500
+
+dimsubs = lambda x: x.subs({n: nn, k: nk})
+inputs = map(dimsubs, inputs)
+outputs = map(dimsubs, outputs)
+assumptions = tuple(map(dimsubs, assumptions))
+
+c = make_kalman_comp(*inputs)
 
 def make_inputs(nn, nk):
     dims = {n: nn, k: nk}
@@ -20,11 +29,15 @@ def make_inputs(nn, nk):
     ninputs = map(np.asfortranarray, ninputs)
     return ninputs
 
-def test_kalman():
-    ninputs = make_inputs(100, 100)
+def compcost_kalman(nn=1000, nk=500):
+    ninputs = make_inputs(nn, nk)
 
     types = tuple(map(Q.real_elements, inputs))
     with assuming(*(assumptions+types)):
         compcost = make_compcost(c, inputs, ninputs)
 
+    return compcost
+
+def test_compcost():
+    compcost = compcost_kalman()
     assert all(isinstance(compcost(comp, 1), float) for comp in c.computations)
