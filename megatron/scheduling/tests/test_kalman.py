c2ef98b5f51aa2f516259bb3dd0b33d69d92178a
Matthew Rocklin <mrocklin@gmail.com>
2013-06-19 08:49:51 -0500

    test tompkins scheduler in kalman

--- megatron/scheduling/tests/test_kalman.py
+++ megatron/scheduling/tests/test_kalman.py
@@ -2,11 +2,12 @@ from sympy import Q, assuming
 from computations.matrices.examples.kalman import (inputs, outputs, n, k,
         assumptions)
 from computations.matrices.examples.kalman_comp import make_kalman_comp
-from megatron.scheduling.times import make_compcost, make_commcost
+from megatron.scheduling.times import (make_compcost, make_commcost,
+        make_commcost_tompkins)
 
 import numpy as np
 
-nn, nk = 1000, 500
+nn, nk = 1000, 1000
 
 dimsubs = lambda x: x.subs({n: nn, k: nk})
 inputs = map(dimsubs, inputs)
@@ -28,7 +29,7 @@ def make_inputs(n, k):
     ninputs = map(np.asfortranarray, ninputs)  # force fortran ordering
     return ninputs
 
-def compcost_kalman(nn=1000, nk=500):
+def compcost_kalman(nn=nn, nk=nk):
     ninputs = make_inputs(nn, nk)
 
     types = tuple(map(Q.real_elements, inputs))
@@ -41,55 +42,86 @@ def test_compcost():
     compcost = compcost_kalman()
     assert all(isinstance(compcost(comp, 1), float) for comp in c.computations)
 
-def make_order_cmp(event_list):
-    jobs = [e.job for e in event_list]
-    def order_cmp(a, b):
-        if a in jobs and b in jobs:
-            return jobs.index(b) - jobs.index(a)
-        else:
-            return 0
-    return order_cmp
-
-
 
 def test_integrative():
-    from heft import schedule, insert_sendrecvs
+    from heft import schedule as schedule_heft
+    from heft import insert_sendrecvs, makespan
+    from tompkins import schedule as schedule_tompkins
+    from tompkins import orderings
     from computations.matrices.mpi import isend, irecv
     from computations.matrices.blas import COPY
     from computations.inplace import inplace_compile
     from computations.matrices.io import disk_io
     from computations.matrices.fortran.mpi import generate_mpi
     from computations.core import CompositeComputation
+    from megatron.scheduling.util import make_order_cmp
+    from megatron.scheduling.tompkinslink import computation_from_dict
     ninputs = make_inputs(nn, nk)
     types = tuple(map(Q.real_elements, inputs))
     mu, Sigma, H, R, data = inputs
     newmu, newSigma = outputs
     filenames = {mu: 'mu.dat', Sigma: 'Sigma.dat', H: 'H.dat', R: 'R.dat',
                  data: 'data.dat', newmu: 'mu2.dat', newSigma: 'Sigma2.dat'}
+    a, b = agents = (0, 1)
 
     with assuming(*(assumptions+types)):
         compcost = make_compcost(c, inputs, ninputs)
-        commcost = make_commcost(1e-6, 1e-9)
-        orders, jobson = schedule(c.dict_io(), (0,1), compcost, commcost)
+
+        # HEFT
+        commcost = make_commcost(7.058e-5, 6.87e-7)
+        orders, jobson = schedule_heft(c.dict_io(), agents, compcost, commcost)
         neworders, jobson = insert_sendrecvs(orders, jobson, c.dict_io(),
                                              send=isend, recv=irecv)
-        c1 = CompositeComputation(*[e.job for e in neworders[0]])
-        c2 = CompositeComputation(*[e.job for e in neworders[1]])
-        c1io = disk_io(c1, filenames)
-        c2io = disk_io(c2, filenames)
-        ic1io = inplace_compile(c1io, Copy=COPY)
-        ic2io = inplace_compile(c2io, Copy=COPY)
-        code = generate_mpi(ic1io, 'c1', [make_order_cmp(orders[0])],
-                            ic2io, 'c2', [make_order_cmp(orders[1])])
-        with open('tmp/kalman_mpi.f90', 'w') as f:
-            f.write(code)
-        assert isinstance(code, str)
+        h0 = CompositeComputation(*[e.job for e in neworders[a]])
+        h1 = CompositeComputation(*[e.job for e in neworders[b]])
+
+        h0io= disk_io(h0, filenames)
+        h1io= disk_io(h1, filenames)
+        ih0 = inplace_compile(h0io, Copy=COPY)
+        ih1 = inplace_compile(h1io, Copy=COPY)
+
+        hcmpa = make_order_cmp([e.job for e in orders[a]])
+        hcmpb = make_order_cmp([e.job for e in orders[b]])
+        heftcode = generate_mpi(ih0, 'h0', [hcmpa], ih1, 'h1', [hcmpb])
+        with open('tmp/kalman_mpi_heft.f90', 'w') as f:
+            f.write(heftcode)
+        assert isinstance(heftcode, str)
+
+        # Tompkins
+        commcost_tomp = make_commcost_tompkins(7.058e-5, 6.87e-7)
+        dags, sched, m = schedule_tompkins(c.dict_io(), agents, compcost,
+                commcost_tomp, send=isend, recv=irecv, M=2.0)
+        torders = orderings(sched)
+        t0 = computation_from_dict(dags[a])
+        t1 = computation_from_dict(dags[b])
+
+        t0io= disk_io(t0, filenames)
+        t1io= disk_io(t1, filenames)
+        it0 = inplace_compile(t0io, Copy=COPY)
+        it1 = inplace_compile(t1io, Copy=COPY)
+
+        tcmpa = make_order_cmp(torders[a])
+        tcmpb = make_order_cmp(torders[b])
+        tompcode = generate_mpi(it0, 't0', [tcmpa],
+                                it1, 't1', [tcmpb])
+        with open('tmp/kalman_mpi_tompkins.f90', 'w') as f:
+            f.write(tompcode)
+        assert isinstance(tompcode, str)
 
     from computations.dot import writepdf
-    writepdf(c1io, 'tmp/kalman_mpi_1')
-    writepdf(c2io, 'tmp/kalman_mpi_2')
+    writepdf(t0io, 'tmp/kalman_mpi_t0')
+    writepdf(t1io, 'tmp/kalman_mpi_t1')
+    writepdf(h0io, 'tmp/kalman_mpi_h0')
+    writepdf(h1io, 'tmp/kalman_mpi_h1')
     with open('tmp/orders.txt', 'w') as f:
         f.write('\n'.join(map(str, orders.items())))
+    with open('tmp/sched.txt', 'w') as f:
+        f.write('\n'.join(map(str, sched)))
+
+    print "Heft     Makespan: ", makespan(orders)
+    print "Tompkins Makespan: ", m
+    assert m <= makespan(orders)
+    assert 2*m >= makespan(orders)
 
 def write_kalman_data(n, k, directory='.'):
     ninputs = make_inputs(n, k)
