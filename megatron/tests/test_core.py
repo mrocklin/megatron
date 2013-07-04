daae7e3beb4c82c5a23a22ee5839a8e98133390a
Matthew Rocklin <mrocklin@gmail.com>
2013-07-04 16:14:47 -0500

    add variables to computation_for test

--- megatron/tests/test_core.py
+++ megatron/tests/test_core.py
@@ -1,16 +1,17 @@
 from sympy import Symbol, MatrixSymbol, ask, Q, assuming, ZeroMatrix
 from computations.matrices.blas import SYRK, GEMM, SYMM
-from megatron.core import computations_for, compile
+from megatron.core import computations_for, compile, vars, variables
 
 n = Symbol('n')
 X = MatrixSymbol('X', n, n)
 expr = X * X.T
 
 def test_correct_computation_matches():
-    assert set(map(type, computations_for(expr))) == set((SYRK, GEMM))
+    with variables(*vars):
+        assert set(map(type, computations_for(expr))) == set((SYRK, GEMM))
 
-    with assuming(Q.symmetric(X)):
-        assert set(map(type, computations_for(expr))) == set((SYRK, GEMM, SYMM))
+        with assuming(Q.symmetric(X)):
+            assert set(map(type, computations_for(expr))) == set((SYRK, GEMM, SYMM))
 
 def test_computations_reify_correctly():
     print computations_for(expr)
