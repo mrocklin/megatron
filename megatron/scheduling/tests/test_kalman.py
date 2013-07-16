7d7501ca12443633d357383c65162cc4346bfb6c
Matthew Rocklin <mrocklin@gmail.com>
2013-07-15 19:04:26 -0500

    factor kalman code out from test into example

--- megatron/scheduling/tests/test_kalman.py
+++ megatron/scheduling/tests/test_kalman.py
@@ -1,42 +1,10 @@
 from sympy import Q, assuming
-from computations.matrices.examples.kalman import (inputs, outputs, n, k,
-        assumptions)
-from computations.matrices.examples.kalman_comp import make_kalman_comp
+from megatron.examples.kalman import (inputs, outputs, n, k,
+        assumptions, c, nn, nk, make_inputs, compcost_kalman)
 from megatron.scheduling.times import (make_compcost, make_commcost,
         make_commcost_tompkins)
 
 import numpy as np
-
-nn, nk = 1000, 1000
-
-dimsubs = lambda x: x.subs({n: nn, k: nk})
-inputs = map(dimsubs, inputs)
-outputs = map(dimsubs, outputs)
-assumptions = tuple(map(dimsubs, assumptions))
-
-c = make_kalman_comp(*inputs)
-
-def make_inputs(n, k):
-    ninputs = [
-        np.ones(n, dtype=np.float64),
-        np.eye(n, dtype=np.float64),
-        np.hstack([np.eye(k, dtype=np.float64),
-                              np.zeros((k, n-k), dtype=np.float64)]),
-        np.eye(k, dtype=np.float64),
-        np.ones(k, dtype=np.float64)
-    ]
-
-    ninputs = map(np.asfortranarray, ninputs)  # force fortran ordering
-    return ninputs
-
-def compcost_kalman(nn=nn, nk=nk):
-    ninputs = make_inputs(nn, nk)
-
-    types = tuple(map(Q.real_elements, inputs))
-    with assuming(*(assumptions+types)):
-        compcost = make_compcost(c, inputs, ninputs)
-
-    return compcost
 
 def test_compcost():
     compcost = compcost_kalman()
